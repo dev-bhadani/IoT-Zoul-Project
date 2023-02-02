b54e0e684e1872de60257504ec868acb54daa4b3
Shaileshkumar Bhadani <123907648+sbhadani98@users.noreply.github.com>
2023-02-02 10:43:20 +0100

    Add files via upload

---
+++ gas-sensor.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "contiki.h"
+#include "dev/leds.h"
+#include "dev/relay.h"
+#include "lib/sensors.h"
+#if CONTIKI_TARGET_ZOUL
+#include "dev/adc-zoul.h"
+#include "dev/zoul-sensors.h"
+#else
+#endif
+
+PROCESS(test_adc_process, "Gas Sensor");
+AUTOSTART_PROCESSES(&test_adc_process);
+
+static struct etimer et;
+
+PROCESS_THREAD(test_adc_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+#if CONTIKI_TARGET_ZOUL
+
+  adc_zoul.configure(SENSORS_HW_INIT, ZOUL_SENSORS_ADC_ALL);
+  SENSORS_ACTIVATE(relay);
+
+#endif
+
+
+  etimer_set(&et, CLOCK_SECOND);
+
+  while (1)
+  {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+#if CONTIKI_TARGET_ZOUL
+
+    
+    int gas_sensor_reading = adc_zoul.value(ZOUL_SENSORS_ADC3);
+    printf("\nGas Sensor Value := %u mV", gas_sensor_reading);
+    int gas_sensor_threshold = 550;
+
+    if(gas_sensor_reading >= gas_sensor_threshold)
+    {
+      leds_toggle(LEDS_RED);
+      relay.value(RELAY_ON);
+      printf("\nGas leakage detected!");
+      printf("\nRelay status : ON");
+      printf("\nLed status : RED\n");
+    }
+    else
+    {
+      leds_toggle(LEDS_GREEN);
+      relay.value(RELAY_OFF);
+      printf("\nGas leakage not detected yet!");
+      printf("\nRelay status : OFF");
+      printf("\nLed status : Green\n");
+    }
+     
+#endif
+    etimer_reset(&et);
+  }
+
+  PROCESS_END();
+}
